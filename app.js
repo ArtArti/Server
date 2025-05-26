9545be78ebf3083bd655b3e084d7177fd6cbedd3
ArtArti <103097814+ArtArti@users.noreply.github.com>
2025-05-26 16:28:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ dbconnect();
 
 // Apply CORS middleware
 cors({
-  origin:["https://note-planner-client.vercel.app/"], 
+  origin:["https://note-planner-client.vercel.app"], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true,
 })
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://note-planner-client.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "https://note-planner-client.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
