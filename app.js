426399a370d624aca02391df6bf9405d61d1c064
ArtArti <103097814+ArtArti@users.noreply.github.com>
2025-05-26 23:09:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dbconnect();
 
 // Apply CORS middleware
 cors({
-  origin:["https://note-planner-client.vercel.app"], 
+  origin:["https://note-planner-client.vercel.app/"], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true,
 })
