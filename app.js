2695dc4d214b9a9ac4f967dc46d042e4c678ef45
ArtArti <103097814+ArtArti@users.noreply.github.com>
2025-05-26 16:22:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ cors({
 })
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://blog-wave.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://note-planner-client.vercel.app/");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
